1e443fa31c8f7a030d7d5e7e2a18d7b46f40685d
Mark Pereira <markpereira17@gmail.com>
2020-06-04 14:46:43 -0400

    removing arc

--- src/App.js
+++ src/App.js
@@ -4,57 +4,13 @@ import './App.css';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-import {getArc} from "./arc";
-
-const Arc = require('@daostack/client').Arc;
-
-// const Arc = require('../dist/lib/index.js').Arc
-
-var util = require('util');
-
-// const graphHttpLink =
-//   'https://api.thegraph.com/subgraphs/name/daostack/v7_5_exp_rinkeby';
-// const graphwsLink =
-//   'wss://api.thegraph.com/subgraphs/name/daostack/v7_5_exp_rinkeby';
-
-const graphHttpLink =
-  'https://api.thegraph.com/subgraphs/name/daostack/alchemy';
-const graphwsLink =
-  'wss://api.thegraph.com/subgraphs/name/daostack/alchemy';
-
-// create an Arc instance
-const arc = new Arc({
-  graphqlHttpProvider: graphHttpLink,
-  graphqlWsProvider: graphwsLink,
-  web3Provider: `wss://rinkeby.infura.io/ws/v3/${'4406c3acf862426c83991f1752c46dd8'}`,
-  ipfsProvider: {
-    "host": "subgraph.daostack.io",
-    "port": "443",
-    "protocol": "https",
-    "api-path": "/ipfs/api/v0/"
-  }
-});
 
 const App = () => {
-  const [daos, setDaos] = useState({});
+  const [daos, setDaos] = useState({aintNoSunshineWhenSheGone: true});
   useEffect(() => {
 
     const getDaos = async () => {
-      console.log('Arc: ', arc);
-      arc
-        .daos({orderBy: 'name', orderDirection: 'asc'}, {fetchAllData: true})
-        .subscribe(res => {
-          res.map(dao => {
-            let daoObject = util.inspect(dao.coreState)
-            setDaos(daoObject);
-            const {name, id, memberCount, tokenName} = dao.coreState;
-            if (name === 'Mango DAO') {
-              console.log('mango', dao)
-            }
-            console.log('2 query : ', dao);
-
-          })
-        });
+      console.log('Arc: ');
     }
     getDaos()
   }, [0]);

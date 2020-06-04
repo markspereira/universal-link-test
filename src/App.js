33e6f917b8599043107e5827a99edb0adf0ebc9b
Mark Pereira <markpereira17@gmail.com>
2020-06-04 14:23:17 -0400

    👆

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import { docco } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-function App() {
+import {getArc} from "./arc";
+
+const Arc = require('@daostack/client').Arc;
+
+// const Arc = require('../dist/lib/index.js').Arc
+
+var util = require('util');
+
+// const graphHttpLink =
+//   'https://api.thegraph.com/subgraphs/name/daostack/v7_5_exp_rinkeby';
+// const graphwsLink =
+//   'wss://api.thegraph.com/subgraphs/name/daostack/v7_5_exp_rinkeby';
+
+const graphHttpLink =
+  'https://api.thegraph.com/subgraphs/name/daostack/alchemy';
+const graphwsLink =
+  'wss://api.thegraph.com/subgraphs/name/daostack/alchemy';
+
+// create an Arc instance
+const arc = new Arc({
+  graphqlHttpProvider: graphHttpLink,
+  graphqlWsProvider: graphwsLink,
+  web3Provider: `wss://rinkeby.infura.io/ws/v3/${'4406c3acf862426c83991f1752c46dd8'}`,
+  ipfsProvider: {
+    "host": "subgraph.daostack.io",
+    "port": "443",
+    "protocol": "https",
+    "api-path": "/ipfs/api/v0/"
+  }
+});
+
+const App = () => {
+  const [daos, setDaos] = useState({});
+  useEffect(() => {
+
+    const getDaos = async () => {
+      console.log('Arc: ', arc);
+      arc
+        .daos({orderBy: 'name', orderDirection: 'asc'}, {fetchAllData: true})
+        .subscribe(res => {
+          res.map(dao => {
+            let daoObject = util.inspect(dao.coreState)
+            setDaos(daoObject);
+            const {name, id, memberCount, tokenName} = dao.coreState;
+            if (name === 'Mango DAO') {
+              console.log('mango', dao)
+            }
+            console.log('2 query : ', dao);
+
+          })
+        });
+    }
+    getDaos()
+  }, [0]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <SyntaxHighlighter language="javascript" style={{...docco, width:  '80%'}}>
+            {JSON.stringify(daos)}
+          </SyntaxHighlighter>Z
+        </div>
       </header>
     </div>
   );
---
+++ src/arc.js
@@ -0,0 +1,55 @@
+const { Arc } = require('@daostack/arc.js')
+const ethers = require('ethers')
+// this value should coincide with the "migration-experimental" versoin
+const ARC_VERSION = '0.1.1-rc.13'
+// const NETWORK = 'rinkeby'
+// const pathToABIs = path.join(require.resolve('@daostack/j'), '..', 'contracts', CONTRACT_VERSION)
+
+
+// // not sure where this comes from ..
+// const CONTRACT_ADDRESSES = require(path.join(require.resolve('@daostack/migration-experimental'), '..', 'migration.json'))
+//     [NETWORK].package[CONTRACT_VERSION]
+
+
+// private key of this address:
+const ADDRESS_1 = '0xea64B1E098432e12c51694648A21c57ACE7621c4'
+const PRIVATE_KEY_1 = '0xD865F557C088E1F7BDFB87D359F9E244C73272BDC39CB7CC1898D7A348A4BF2C'
+
+const ADDRESS_2 = '0xb8a15CD235b34F52D5FeD7155dF2C07DE594e03e'
+const PRIVATE_KEY_2 = '0x015ECB47E92655A52A0A3ECF38D1CD3F4599CD09166A0FAC48F507F341F7C0FF'
+
+async function getArc() {
+
+  const web3Provider = new ethers.providers.JsonRpcProvider( `https://rinkeby.infura.io/v3/e0cdf3bfda9b468fa908aa6ab03d5ba2`)
+  const infuraProvider = new ethers.providers.InfuraProvider('rinkeby', 'e0cdf3bfda9b468fa908aa6ab03d5ba2')
+  const wallet = new ethers.Wallet(PRIVATE_KEY_2, infuraProvider);
+  console.log('here: ->', wallet)
+
+  const arc = new Arc({
+    graphqlHttpProvider: "https://api.thegraph.com/subgraphs/name/daostack/v7_4_exp_rinkeby",
+    graphqlWsProvider: "wss://api.thegraph.com/subgraphs/name/daostack/v7_4_exp_rinkeby",
+    web3Provider: web3Provider
+  })
+  await arc.fetchContractInfos()
+
+  arc.web3 = infuraProvider
+
+  return arc
+}
+
+const OVERRIDES =  {
+  gasLimit: 10000000,
+  gasPrice: 1000000000,
+}
+
+module.exports = {
+  ARC_VERSION,
+  ADDRESS_1,
+  PRIVATE_KEY_1,
+  ADDRESS_2,
+  PRIVATE_KEY_2,
+  getArc,
+  // pathToABIs,
+  // CONTRACT_ADDRESSES,
+  OVERRIDES
+}
